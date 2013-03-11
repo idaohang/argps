f35f16a666cf508fc92de35a9a875fef5b40c85d
Keeler <keeler@3001-11.eecs.ku.edu>
2013-03-11 14:47:30 -0400

    Implemented gpsd API calls in client. Not tested.

--- argps.c
+++ argps.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <math.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include <stdio.h>
@@ -15,6 +16,8 @@
 #include <string.h>
 #include <stdio.h>
 
+#include <gps.h>
+
 #define PORT_NUMBER "12345"
 #define MAX_BUFFER_SIZE 1024
 
@@ -23,30 +26,81 @@ int createConnection( const char *hostname, const char *port );	// port number a
 
 int main( int argc, char **argv )
 {
+	// Enforce proper command line arguments.
+	if( argc != 2 )
+	{
+		fprintf( stderr, "Usage: %s <hostname>\n", argv[0] );
+		exit( EXIT_FAILURE );
+	}
+
+	// Connect to remote host.
 	int sockfd = createConnection( argv[1], PORT_NUMBER );
 	if( sockfd < 0 )
 	{
-		fprintf( stderr, "Couldn't connect, terminating.\n" );
+		fprintf( stderr, "Couldn't connect to %s.\n", argv[1] );
+		exit( EXIT_FAILURE );
 	}
 
-	while( 1 )
+	// Connect to gpsd.
+	static struct gps_data_t *GPS = NULL;
+	if( gps_open( "127.0.0.1", "2947", GPS ) < 0 )
 	{
-		char *buffer = NULL;
-		printf( ">> " );
-		getLine( buffer );
+		fprintf( stderr, "Couldn't connect to gpsd.\n" );
+		exit( EXIT_FAILURE );
+	}
 
-		if( buffer == NULL )
+	// Register for updates from gpsd.
+	gps_stream( GPS, WATCH_ENABLE | WATCH_JSON, NULL );
+
+	printf( "Waiting for gps lock." );
+	while( GPS->status == 0 )
+	{
+		if( gps_waiting( GPS, 500 ) )
 		{
-			break;
+			// Error check the GPS data read.
+			if( gps_read( GPS ) == -1 )
+			{
+				fprintf( stderr, "gps_read() error, terminating.\n" );
+				gps_stream( GPS, WATCH_DISABLE, NULL );
+				gps_close( GPS );
+				close( sockfd );
+				exit( EXIT_FAILURE );
+			}
+			else
+			{
+				// Status > 0 means data is present.
+				if( GPS->status > 0 )
+				{
+					if( isnan( GPS->fix.latitude ) || isnan( GPS->fix.longitude ) )
+					{
+						fprintf( stderr, "Couldn't get GPS fix.\n" );
+						gps_stream( GPS, WATCH_DISABLE, NULL );
+						gps_close( GPS );
+						close( sockfd );
+						exit( EXIT_FAILURE );
+					}
+					else
+					{
+						printf( "\n" );
+						printf( "latitude: %f\n", GPS->fix.latitude );
+						printf( "longitude: %f\n", GPS->fix.longitude );
+					}
+				}
+				else
+				{
+					printf( "." );
+				}
+			}
 		}
-		int numSent;
-		if( ( numSent = send( sockfd, buffer, strlen( buffer ) + 1, 0 ) ) == -1 )
+		// If gps_waiting() returns false, gpsd has shut the stream down (done
+		// periodically). Re-open the stream to continue reading GPS data.
+		else
 		{
-			fprintf( stderr, "send() failure.\n" );
-			exit( EXIT_FAILURE );
+			gps_stream( GPS, WATCH_ENABLE | WATCH_JSON, NULL );
 		}
 
-		free( buffer );
+		// Sleep so we're not sending too much data.
+		sleep( 1 );
 	}
 
 	close( sockfd );
@@ -129,3 +183,4 @@ int createConnection( const char *hostname, const char *port )
 
 	return sockfd;
 }
+
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 argps: argps.o
-	g++ -O3 -Wall -g -o argps argps.o
+	g++ -O3 -Wall -g -lgps -o argps argps.o
 
 argps.o: argps.c
-	g++ -O3 -Wall -g -c argps.c
+	g++ -O3 -Wall -g -lgps -c argps.c
 
 clean:
 	rm -f argps argps *.o
